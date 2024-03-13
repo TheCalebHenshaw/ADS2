236247e4d9bf3751e17f4b8b7cff3571378b34e4
Caleb Henshaw <2810458h@student.gla.ac.uk>
2024-03-13 14:54:03 +0000

    more done

--- ADSAE2/src/ae2/BSTHeightHistogram.java
+++ ADSAE2/src/ae2/BSTHeightHistogram.java
@@ -10,7 +10,37 @@ import java.util.Scanner;
 public class BSTHeightHistogram {
     
 
+
+
+    public static void printHistogram(int n){
+        float avgHeight;
+        System.out.println("Height     Frequency");
+        System.out.println("----------------------");
+        for(int i = 0; i < n; i++){
+            System.out.println(i+"       int result goes here");
+        }
+        System.out.println("\n" +"Average height of BSTs:" + " some float here");
+    }
+
     public static void main(String[] args){
-        System.out.println("this should be run");
+        int n = getInput();
+
+        printHistogram(n);
+    }
+    public static int getInput(){
+
+        try(Scanner scan = new Scanner(System.in)){
+            System.out.println("Enter a positive integer n:");
+            int toReturn = scan.nextInt();
+            if(toReturn<0){
+                System.out.println("Please enter a positive integer");
+                toReturn = getInput();
+            }
+            return toReturn;
+        }
+
+    }
+    public static int getBSTHeight(){
+        return 0;
     }
 }
